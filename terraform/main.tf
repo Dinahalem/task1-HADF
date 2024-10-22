2469b737f1087368e4b54dfb04b221eb61ac9703
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-10-22 13:32:37 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 # Define an ECS Instance
 resource "alicloud_instance" "main_instance" {
   instance_name = "TST-infra-App05"
-  instance_type = "ecs.c6.large"      
+  instance_type = var.alicloud_instance_type      
   key_name = "mykey"
-  image_id      = var.alicloud_image_id     # Use the official Alibaba Cloud images, e.g., Ubuntu, CentOS, etc.
+  image_id      = var.alicloud_image_id     
   security_groups = ["${alicloud_security_group.main_sg}"]
 }
