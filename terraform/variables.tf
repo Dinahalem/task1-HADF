25ea2f82f98dda527e2d5f1673fe36847fd9a2fb
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-10-22 13:31:56 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,6 +8,11 @@ variable "alicloud_image_id" {
   default = "win2022_21H2_x64_dtc_en-us_40G_alibase_20230915.vhd" 
 }
 
+variable "alicloud_instance_type" {
+  type    = string
+  default = "ecs.c6.large" 
+}
+
 variable "instance_password" {
   description = "Password for the instance"
   type        = string
