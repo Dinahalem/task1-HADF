4100e4b07ffd9c1c9e29cabf0583e14ef9e47528
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-10-22 13:46:04 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,13 @@ resource "alicloud_instance" "main_instance" {
   instance_name = "TST-infra-App05"
   instance_type = var.alicloud_instance_type      
   key_name = "mykey"
-  image_id      = var.alicloud_image_id     
+  image_id = var.alicloud_image_id  
+  system_disk_category = "cloud_efficiency" 
+  system_disk_size = 40  
   security_groups = ["${alicloud_security_group.main_sg}"]
   
-  zone_id               = "me-east-1-a"  
-  vswitch_id            = alicloud_vswitch.vswitch.id
+  zone_id = "me-east-1-a"  
+  vswitch_id = alicloud_vswitch.vswitch.id
 
   cpu = 2
   memory = 4
